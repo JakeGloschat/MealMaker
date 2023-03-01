df7b4f3de5939f37ecc6e70176ed1098403b6117
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-03-01 09:45:35 -0700

    Started initial project, organized my file structure.  Created Models and created networking constants.

---
+++ MealMaker/MealMaker/Models/Meal.swift
@@ -0,0 +1,24 @@
+//
+//  Meal.swift
+//  MealMaker
+//
+//  Created by Jake Gloschat on 3/1/23.
+//
+
+import Foundation
+
+struct MealToplevelDictionary: Decodable {
+    let meals: [Meal]
+}
+
+struct Meal: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case mealname = "strMeal"
+        case mealImageURL = "strMealThumb"
+        case mealID = "idMeal"
+    }
+    
+    let mealName: String
+    let mealImageURL: String
+    let mealID: String
+}
