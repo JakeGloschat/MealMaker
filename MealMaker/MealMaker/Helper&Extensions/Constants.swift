a8e6035ab3f587bc522d4a7e3b2b02af656cd173
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-03-01 11:05:46 -0700

    Finished CategoryTableVC and mostly finished the MealListVC.

--- MealMaker/MealMaker/Helper&Extensions/Constants.swift
+++ MealMaker/MealMaker/Helper&Extensions/Constants.swift
@@ -10,8 +10,12 @@ import Foundation
 struct Constants {
     
     struct MealService {
-        static let allCategoriesBaseURL = "www.themealdb.com/api/json/v1/1/categories.php"
-        static let mealsInCategoryBaseURL = "www.themealdb.com/api/json/v1/1/filter.php"
+        static let allCategoriesBaseURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+        static let mealsInCategoryBaseURL = "https://www.themealdb.com/api/json/v1/1/filter.php"
         static let categoryQueryKey = "c"
+    }
+    
+    struct Error {
+        static let unkownError = "Unknown Error. Call Chase Hill at 867-5309"
     }
 }
---
+++ MealMaker/MealMaker/ViewControllers/MealListTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MealListTableViewController.swift
+//  MealMaker
+//
+//  Created by Jake Gloschat on 3/1/23.
+//
+
+import UIKit
+
+class MealListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchMealsInCategory()
+    }
+    
+    // MARK: - Properties
+    var category: Category?
+    var mealArray: [Meal] = []
+    
+    
+    // MARK: - Functions
+    func fetchMealsInCategory() {
+        guard let category = category else { return }
+        MealService.fetchmealsInCategory(forCategory: category) { [weak self] result in
+            switch result {
+            case .success(let meals):
+                self?.mealArray = meals
+                DispatchQueue.main.async { self?.tableView.reloadData() }
+            case .failure(let error):
+                print(error.errorDescription ?? Constants.Error.unkownError)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mealArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mealCell", for: indexPath)
+
+        let meal = mealArray[indexPath.row]
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = meal.mealName
+        config.secondaryText = meal.mealID
+        cell.contentConfiguration = config
+
+        return cell
+    }
+  
+  
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
